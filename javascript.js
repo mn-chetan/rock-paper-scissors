405a4ded96a465b28ae0eb0824292b6da795b8c3
Chetan <165123379+mn-chetan@users.noreply.github.com>
2024-10-09 21:31:23 +0530

    Reset js code

--- javascript.js
+++
@@ -1,7 +0,0 @@
-function getComputerChoice() {
-  const choice = Math.random();
-  // r->ROCK, p->PAPER, s->Scissors
-  if (choice < 1 / 3) return "r";
-  else if (choice < 2 / 3) return "p";
-  else return "s";
-}
