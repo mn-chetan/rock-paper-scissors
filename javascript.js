aad7e687ffaa5a8bcced0adeea05378fe632386a
Chetan <165123379+mn-chetan@users.noreply.github.com>
2024-10-10 16:08:00 +0530

    Let player choose from rock, paper or scissors
    -Player's choice is displayed on the screen
    -After player makes a choice, computer randomly
     chooses

--- javascript.js
+++ javascript.js
@@ -5,3 +5,23 @@ function getComputerChoice() {
   else if (randomNumber < 2 / 3) return "Paper";
   else return "Scissors";
 }
+
+// function to let user choose of one of the buttons
+function getHumanChoice() {
+  const buttons = document.querySelectorAll(".choice button");
+  buttons.forEach((button) =>
+    button.addEventListener("click", (e) => {
+      // insert the player's choice on screen
+      const player = document.querySelector(".player");
+      const paraPlayer = document.createElement("p");
+      paraPlayer.textContent = e.target.textContent;
+      player.appendChild(paraPlayer);
+
+      // after player has chosen, randomly generate rock, paper or scissor as computer choice
+      const computer = document.querySelector(".computer");
+      const paraComputer = document.createElement("p");
+      paraComputer.textContent = getComputerChoice();
+      computer.appendChild(paraComputer);
+    })
+  );
+}
